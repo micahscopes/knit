03151f19dc5ae252baf571bc5bc0d93605a9f175
Micah <fitchmicah@gmail.com>
2016-08-14 13:53:15 -0500

    support for Hash-like objects that aren't Hashes

--- lib/knit.rb
+++ lib/knit.rb
@@ -2,13 +2,16 @@ require "knit/version"
 
 module Knit
   module Hash
+    public
     def knit(other)
      return self if other == nil
-     return other.knit(self) if !other.is_a?(Hash)
+     return other.knit(self) if !other.respond_to?(:keys)
      self.merge(other) do |key, a, b|
        knit = a if b.is_a? NilClass
        knit = b if a.is_a? NilClass
-       knit = if a.is_a?(Hash) && b.is_a?(Hash)
+       knit = if a.respond_to?(:keys) && b.respond_to?(:keys)
+         a.to_h.knit(b.to_h)
+       elsif a.respond_to?(:knit) && b.respond_to?(:knit)
          a.knit(b)
        else
          [a,b].flatten(1)
@@ -29,10 +32,11 @@ module Knit
   module Array
     def knit(other)
       unless other.respond_to? :keys
-        knit = self+[other].flatten(1)
+        knit = self+[other].flatten(1).map{|a| a.to_h if a.respond_to? :keys}
       else
-        knit = ([self]+[other]).flatten(1)
-        knit = knit.partition{|obj| obj.is_a?(Hash)}
+        other = other.to_h
+        knit = ([self]+[other]).flatten(1) #.map{|a| a.to_h if a.respond_to? :keys}
+        knit = knit.partition{|obj| obj.respond_to? :keys}
         knit = [knit[1],knit[0].reduce{|a,b| a.knit(b)}].flatten(1)
       end
       if block_given?
